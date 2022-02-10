e23f4030ac796df64e45bcede57f21c28a49c9bf
Seale <1801157108@qq.com>
2022-02-10 21:52:40 +0800

    [init]初始化项目

---
+++ app/src/main/java/com/eendtech/cqutsmartwatchapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.eendtech.cqutsmartwatchapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        tvTitle.setText("测试设置");
+    }
+}
